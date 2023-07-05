bb358a8cce24176efd1cec11f2eb65b4c4631931
hwaniee <yhlee@xinapse.ai>
2023-07-05 16:48:10 +0900

    Dockerfile 추가

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-@RequiredArgsConstructor
+/*@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final AccountTestService accountTestService;
-    /*@GetMapping("/")
+    *//*@GetMapping("/")
     public String hello(Model model){
         return "index";
-    }*/
+    }*//*
 
     void pln(String str) {
         System.out.println(str);
     }
 
-}
+}*/
